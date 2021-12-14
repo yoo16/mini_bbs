18bb3f2e831850f5e460352df931dbe325a863f8
yoo16 <yohei.yoshikawa@gmail.com>
2021-12-14 12:30:09 +0900

    投稿画面追加

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="h1">ひとこと掲示板</h1>
+        <form action="" method="post">
+            <dl>
+                <dd>
+                    <textarea 
+                    name="message" 
+                    class="form-control" 
+                    placeholder="いまどうしてる？"
+                    ></textarea>
+                </dd>
+            </dl>
+            <div>
+                <button class="btn btn-primary">投稿</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
